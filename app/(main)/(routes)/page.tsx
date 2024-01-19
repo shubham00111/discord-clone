c7853cb0450d3787afa7b39e0f1a8b13fd959182
Shubham Rawat <shubhamb23efd@gmail.com>
2024-01-20 02:05:09 +0530

    added sign-in and sign-up pages using clerk and added some models for entities using prisma(ORM)

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <p className="text-3xl">this is a protected route</p>;
+  return (
+    <p className="text-3xl">
+      <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
+    </p>
+  );
 }
